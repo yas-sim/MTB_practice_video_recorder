2d219068b9bf544b49f3bece9918235e3e85efa5
Yasunori Shimura <yasu0710@gmail.com>
2021-07-07 11:09:27 +0900

    support movie file

--- mtb_recorder.py
+++ mtb_recorder.py
@@ -1,21 +1,27 @@
-import copy
-import time
+import sys
+
 import datetime
 import numpy as np
 import cv2
 
-cam_width  = 800
-cam_height = 600
-cam_fps = 15
 
-cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FPS, cam_fps)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH,  cam_width)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_height)
-fps = cam.get(cv2.CAP_PROP_FPS)
+if len(sys.argv) > 1:
+    cam = cv2.VideoCapture(sys.argv[1])
+    cam_fps    = int(cam.get(cv2.CAP_PROP_FPS))
+    cam_width  = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    cam_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cam_fps
+else:
+    cam_width  = 800
+    cam_height = 600
+    cam_fps = 15
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FPS, cam_fps)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH,  cam_width)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_height)
+    fps = cam.get(cv2.CAP_PROP_FPS)
 print(fps, 'fps')
 
-stime = time.time()
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 60]
 record_length = 60 * 10   # buffer length [sec]
 movie_length = 5          # movie rength [sec]
@@ -28,7 +34,8 @@ ring_buf = [ zero_enc for dmy in range(int(cam_fps * record_length)) ]
 ptr_w = 0
 ptr_r = 0
 
-ptr_inc = 1  # Read pointer increment value (0=pause, -1=reverse)
+ptr_inc = 1  # Read pointer increment value (1=normal, -1=reverse)
+ptr_mul = 1  # Multiplier for read pointer incrementer (0=pause, 1=play)
 
 def add_ptr(ptrval:int, delta:int):
     global ring_buf
@@ -39,16 +46,24 @@ def add_ptr(ptrval:int, delta:int):
         ptrval -= len(ring_buf)
     return ptrval
 
+prev_sts = True
+
 while True:
-    _, img = cam.read()
-    _, encimg = cv2.imencode('.jpg', img, encode_param)
-    ring_buf[ptr_w] = encimg
-    ptr_w = add_ptr(ptr_w, 1)
-    cv2.imshow('Real Time', img)
+    sts, img = cam.read()
+    if sts:
+        _, encimg = cv2.imencode('.jpg', img, encode_param)
+        ring_buf[ptr_w] = encimg
+        ptr_w = add_ptr(ptr_w, 1)
+        cv2.imshow('Real Time', img)
+    else:
+        if prev_sts == True:
+            ptr_mul = 0                         # pause at the end of the input movie
+            ptr_r = add_ptr(ptr_r, -1)          # rewind 1 frame
+    prev_sts = sts
 
     playback_img = ring_buf[ptr_r]
     decimg = cv2.imdecode(playback_img, 3)
-    ptr_r = add_ptr(ptr_r, ptr_inc)
+    ptr_r = add_ptr(ptr_r, ptr_inc * ptr_mul)
 
     if ptr_w >= ptr_r:
         time_shift = ptr_w - ptr_r
@@ -65,20 +80,24 @@ while True:
         cv2.putText(decimg, msg, pos, cv2.FONT_HERSHEY_PLAIN, 2, (  0,255,255), 1, cv2.LINE_AA)
     cv2.imshow('Time Shift', decimg)
 
-    key = cv2.waitKey(1)
+    key = cv2.waitKey(int(1 * ((1000/cam_fps) if sts==False else 1)))     # frame wait if the input movie has reached to the end
     if key == ord('z'): ptr_r = add_ptr(ptr_r, -cam_fps)
     if key == ord('x'): ptr_r = add_ptr(ptr_r,  cam_fps)
     if key == ord('a'): ptr_r = add_ptr(ptr_r, -cam_fps*10)
     if key == ord('s'): ptr_r = add_ptr(ptr_r,  cam_fps*10)
-    if key == ord('k'): ptr_r = add_ptr(ptr_r, -1)
-    if key == ord('l'): ptr_r = add_ptr(ptr_r,  1)
+    if key == ord('j'): ptr_r = add_ptr(ptr_r, -1)
+    if key == ord('k'): ptr_r = add_ptr(ptr_r,  1)
 
     if key == ord('0'):
         ptr_r = ptr_w
-    if key == ord(' ') or key == ord('p'):
-        ptr_inc = 1 if ptr_inc != 1 else 0
-    if key == ord('o'):
+    if key == ord(' '):
+        ptr_mul = 0 if ptr_mul != 0 else 1
+    if key == ord('m'):
+        ptr_inc = 1
+        ptr_mul = 1
+    if key == ord('n'):
         ptr_inc = -1
+        ptr_mul =  1
 
     if key == ord('r'):
         dt = datetime.datetime.now()
@@ -93,4 +112,5 @@ while True:
     if key == 27 or key == ord('q'):
         break
 
+cam.release()
 cv2.destroyAllWindows()
